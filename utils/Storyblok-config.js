b9d02555f5bfa4f7dd667d63e87d2c05dd269fb0
daniyalinayat <daniyal.inayatullah@virtual-identity.com>
2022-08-23 17:24:29 +0200

    chore: file cleaned

--- utils/Storyblok-config.js
+++ utils/Storyblok-config.js
@@ -1,9 +1,6 @@
 import StoryblokClient from "storyblok-js-client";
 
-// 2. Initialize the client with the preview token
-// from your space dashboard at https://app.storyblok.com
 let Storyblok = new StoryblokClient({
-	// accessToken: process.env.ACCESS_TOKEN, //Sample Space
 	oauthToken: "oEUkARZp6T0hsoGu5MzQoAtt-93801-ThmkXinfFu3HiGeYzNku",
 });
 
